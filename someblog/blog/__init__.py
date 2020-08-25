b8d6002cd0efac670c52dd142d30967b046c5d6b
ApppleTurnover <apppleturnover@gmail.com>
2020-08-26 00:50:55 +1000

    Refactoring + Starter application stage
    Added a navbar and a couple more things, now the site looks like a site.

--- someblog/blog/__init__.py
+++ someblog/blog/__init__.py
@@ -1,6 +1,20 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_navigation import Navigation
+
+# from .navbar import nav, DarkRenderer
+# from flask_nav import register_renderer
 
 from someblog.blog.config.config import TestBuild
 
 app = Flask(__name__)
 app.config.from_object(TestBuild)
+Bootstrap(app)
+
+# Пусть пока побудет тут костыль, может так и останется
+nav = Navigation(app)
+from .navbar import nav
+
+# Не трогать
+# nav.init_app(app)
+# register_renderer(app, 'dark', DarkRenderer)
--- someblog/blog/config/config.py
+++ someblog/blog/config/config.py
@@ -1,3 +1,4 @@
 class TestBuild(object):
     DEBUG = True
-    SERVER_NAME = "192.168.1.42:5000"
+    SERVER_NAME = "127.0.0.1:5000"
+    SEND_FILE_MAX_AGE_DEFAULT = 0
